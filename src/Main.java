2b12191f6cc2dbdee04b0d7a74f6799e8bdd392e
MikiKru <michal_kruczkowski@o2.pl>
2020-06-27 10:49:08 +0200

    jb -v4

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,17 @@ public class Main {
         // utowrzenie obiektu
         User u1 = new User();
         User u2 = new User();
-        // odwołanie do pól klasowych
-        u1.name = "Michał";
-        u2.name = "Anna";
+        // wywołanie setterów
+        u1.setName("Michał");
+        u2.setName("Anna");
+        u2.setGender('K');
         // wyołanie metody toString() na obiekcie u1
         System.out.println(u1.toString());
         System.out.println(u2.toString());
-        System.out.println("User 1: " + u1.name);
-        System.out.println("User 2: " + u2.name);
+        // wywołanie getterów
+        System.out.println("User 1: " + u1.getName());
+        System.out.println("User 2: " + u2.getName());
+        System.out.println("User 2: " + u2.getGender());
 
     }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,19 +1,70 @@
 package model;
 import java.time.LocalDateTime;
-
+// Model JavaBeans - wzorzec struktury składowych klasy
+// 1. Wszystkie pola są prywatne
+// 2. Publiczne gettery i settery - metody dostępowe
 // Klasa User - to klasa modelu -> klasa determinująca strukturę danych
 public class User extends Object{   // Każda klasa dziedziczy - prejmuje pola i metody klasy nadrzędnej
                                     // (zgodnie ze specyfikatorami dostępu)
     // pola klasowe -> pola o zasięgu widoczności w obrębie całej klasy
-    int userId;             // domyślnie 0
-    public String name;            // domyślnie null
-    String lastName;        // domyślnie null
-    String email;           // domyślnie null
-    String password;        // domyślnie null
-    char gender;            // domyślnie ''
-    LocalDateTime registratationDateTime;   // domyślnie null
-    boolean status;         // domyślnie false
+    private int userId;             // domyślnie 0
+    private String name;            // domyślnie null
+    private String lastName;        // domyślnie null
+    private String email;           // domyślnie null
+    private String password;        // domyślnie null
+    private char gender;            // domyślnie ''
+    private LocalDateTime registratationDateTime;   // domyślnie null
+    private boolean status;         // domyślnie false
 
+    // Gettery i Settery
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public LocalDateTime getRegistratationDateTime() {
+        return registratationDateTime;
+    }
+    public void setRegistratationDateTime(LocalDateTime registratationDateTime) {
+        this.registratationDateTime = registratationDateTime;
+    }
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
     // public - widoczna w całym programie
     // String - typ zwracanej wartości w return
     // toString() - nazwa metody (nie przyjmuje argumentów)
