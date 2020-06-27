7904a45aee74b95bade6a5a22185a1312aa2a74c
MikiKru <michal_kruczkowski@o2.pl>
2020-06-27 12:59:56 +0200

    jb -v8

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -14,17 +14,37 @@ public class UserController implements UserControllerTemplate{
         System.out.println("Zarejestrowano nowego użytkownika: " + user.toString());
     }
     @Override
+    public void printAllUsers() {
+        // 1. W pętli wypisz wszystkie elementy
+//        for(int i=0; i<users.size(); i++){          // WYTYCZNE: stosujemy gdy znana jest liczba iteracji
+//            System.out.println(users.get(i));
+//        }
+//        int i = 0;
+//        while (i < users.size()){                   // WYTYCZNE: sosujemy gdy nieznana jest liczba iteracji
+//            System.out.println(users.get(i));
+//            i ++;
+//        }
+//        for(User user : users){                     // for(TypElementuKolekcji nazwaTymczasowegoObiektu : kolekcja)
+//            System.out.println(user);               // WYTYCZNE: stosujemy do kolekcji, ale tylko do odczytu
+//        }
+        // CTRL + Space -> utwórz predykat -> przekształcenie
+        users.forEach(user -> System.out.println(user));
+//        users.forEach(System.out::println);         // jeszcze krócej
+    }
+    @Override
     public boolean loginUser(String email, String password) {
         return false;
     }
     @Override
     public void updatePasswordByUserId(int userId, String newPassword) { }
-    @Override
-    public void printAllUsers() { }
+
 
     public static void main(String[] args) {
         UserController uc = new UserController();
         uc.registerUser(new User("X","X","x@x.pl","x",'K'));
         uc.registerUser(new User("Y","Y","y@y.pl","y",'M'));
+        uc.registerUser(new User("Z","Z","z@z.pl","z",'M'));
+        System.out.println("Wypisanie wszystkich użytkowników:");
+        uc.printAllUsers();
     }
 }
