34d58c32ca6d6c1eeae19894b026dea2e160bdb1
MikiKru <michal_kruczkowski@o2.pl>
2020-06-27 10:59:18 +0200

    jb -v5

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,20 @@ public class Main {
         System.out.println("User 1: " + u1.getName());
         System.out.println("User 2: " + u2.getName());
         System.out.println("User 2: " + u2.getGender());
-
+        // porównanie obiektów
+        System.out.println("Czy obiekty są takie same? "
+                + u1.equals(u2));
+        User u3 = new User();   // inicjalizuje nowy obiekt
+        u3.setName("Michał");
+        System.out.println(u1.toString());
+        System.out.println(u3.toString());
+        System.out.println("Czy obiekty są takie same? "
+                + u1.equals(u3));
+        User u4 = u1;           // obiekt inicjalizowany istejącą instancją
+        System.out.println("Czy obiekty są takie same? "
+                + u1.equals(u4));
+        u4.setEmail("test@test.pl");
+        System.out.println(u1.toString());
+        System.out.println(u4.toString());
     }
 }
